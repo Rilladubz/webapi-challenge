cbc5367ca9517a7902c329309a41d709e472fbdd
Samuel Torres <42902403+Rilladubz@users.noreply.github.com>
2019-12-06 17:04:48 -0500

    MVP

--- data/API/actionsRouter.js
+++ data/API/actionsRouter.js
@@ -1,17 +1,31 @@
 const express = require("express");
 const actionModel = require("../helpers/actionModel");
+const projectModel = require("../helpers/projectModel");
 
 const router = express.Router();
 
 // Request Handler Functions...
 
 // CREATE...
-router.put("/", validateExistingPostID, (req, res) => {
+router.post("/", validateExistingPostID, (req, res) => {
   const body = req.body;
+
   actionModel
-    .insert()
-    .then()
-    .catch();
+    .insert(body)
+    .then(action => {
+      if (!action.description || action.notes < 1) {
+        res
+          .status(400)
+          .json({ errorMessage: "A description & notes are required" });
+      } else {
+        res.status(201).json({ action });
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ errorMessage: "Error while trying to create action" });
+    });
 });
 
 // READ...
@@ -21,7 +35,11 @@ router.get("/:id", (req, res) => {
   actionModel
     .get(id)
     .then(actions => {
-      res.status(200).json({ actions });
+      if (actions) {
+        res.status(200).json({ actions });
+      } else {
+        res.status(404).json({ errorMessage: "action not found invalid id" });
+      }
     })
     .catch(err => {
       res
@@ -32,23 +50,56 @@ router.get("/:id", (req, res) => {
 });
 
 // UPDATE...
-
-// DELETE...
-
-// Middleware...
-function validateExistingPostID(req, res, next) {
-  const id = req.params.id;
+router.put("/:id/update", (req, res) => {
   actionModel
-    .get(id)
-    .then(action => {
-      if (action.id) {
-        next();
-      }
+    .update(req.params.id, req.body)
+    .then(updated => {
+      res.status(201).json({ updated });
     })
     .catch(err => {
       res
-        .status(404)
-        .json({ errorMessage: `The id with a value of ${id} doesn't exist` });
+        .status(500)
+        .json({ errorMessage: "Error occured when trying to update" });
+    });
+});
+
+// DELETE...
+router.delete("/:id/delete", (req, res) => {
+  const id = req.params.id;
+  //   console.log("inbound ID:", id);
+  actionModel
+    .remove(id)
+    .then(item => {
+      res.status(200).json({ item });
+    })
+    .catch(err => {
+      res.status(500).json({
+        errorMessage: "Error while attempting to delete an item",
+        err
+      });
+    });
+});
+
+// Middleware...
+function validateExistingPostID(req, res, next) {
+  const id = req.body.project_id;
+
+  projectModel
+    .get(id)
+    .then(project => {
+      console.log("The project:", project);
+      if (project) {
+        next();
+      } else if (!project) {
+        res.status(404).json({
+          errorMessage: `ProjectId: ${id} Does not exist. An id of an existing project is required.`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        errorMessage: "Error while validating to see if project ID exists"
+      });
     });
 }
 
--- data/API/projectsRouter.js
+++ data/API/projectsRouter.js
@@ -7,19 +7,87 @@ const router = express.Router();
 
 // Request Handler Functions...
 
+// CREATE...
+router.post("/", validateProjectBody, (req, res) => {
+  const body = req.body;
+  console.log("PROJECT BODY:", body);
+  projectModel
+    .insert(body)
+    .then(project => {
+      if (project) {
+        res.status(201).json({ project });
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ errorMessage: "Sorry error occured check with back end", err });
+    });
+});
+
+// READ...
 router.get("/:id", (req, res) => {
   const id = req.params.id;
   console.log("GET ID:", id);
   projectModel
     .get(id)
     .then(proj => {
-      res.status(200).json({ proj });
+      if (proj) {
+        res.status(200).json({ proj });
+      } else {
+        res.status(404).json({
+          errorMessage: `A project with the id of ${id} does not exist.`
+        });
+      }
     })
     .catch(err => {
       res.status(500).json({ errorMessage: "Error while retreving projects" });
     });
 });
 
+// UPDATE...
+router.put("/:id/update", validateProjectBody, (req, res) => {
+  const id = req.params.id;
+  console.log("Update Id:", id);
+  projectModel
+    .update(req.params.id, req.body)
+    .then(updatedProject => {
+      updatedProject
+        ? res.status(201).json({ updatedProject })
+        : res.status(400).json({ errorMessage: "Body is required." });
+    })
+    .catch(err => {
+      res.status(500).json({
+        errorMessage: "Failed Update attempt check with Back end.",
+        err
+      });
+    });
+});
+
+// DELETE...
+router.delete("/:id/delete", (req, res) => {
+  const id = req.params.id;
+  projectModel
+    .remove(id)
+    .then(item => {
+      item
+        ? res.status(200).json({ item })
+        : res
+            .status(500)
+            .json({
+              errorMessage: `A Project with an id of ${id} does not exist`
+            });
+    })
+    .catch();
+});
+
 // Middleware...
+function validateProjectBody(req, res, next) {
+  const body = req.body;
+
+  body.name && body.description
+    ? next()
+    : res.status(400).json({ errorMessage: "Body content required" });
+}
 
 module.exports = router;
